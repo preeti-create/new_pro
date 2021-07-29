2eeebfd678f703c6e5854b5dd70678e12b93ca13
preeti-create <preetibarnwal31@gmail.com>
2021-07-29 12:17:37 +0530

    signup page is done

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,6 +1,54 @@
+const User = require('../models/user');
+
 module.exports.profile = function(req, res){
     return res.render('user_profile', {
         title : "User Profile"
     });
+}
+
+// render the sign up page
+module.exports.signUp = function(req, res){
+    return res.render('user_sign_up', {
+        title : "Codial | Sign Up"
+    })
+}
+
+// render the sign in page
+module.exports.signIn = function(req, res) {
+    return res.render('user_sign_in', {
+        title: "Codial | Sign In"
+    })
+};
+
+module.exports.create = function(req, res)
+{
+    if(req.body.password != req.body.confirm_passwword){
+      res.redirect('back');
+    }
+    User.findOne({email: req.body.email}, function(err, user){
+    if(err){
+        console.log('error in finding user in signing up', {err});
+        return ;
+    }
+
+    if(!user){
+     User.create(req.body, function(err, user){
+        if(err){
+            console.log('error in finding user in signing up', {err});
+            return ;
+        }
+        res.redirect('/users/sign-in');
+     });
+
+    }else{
+        res.redirect('back');
+    }
+    });
+    // todo later
+}
+
+module.exports.createSession = function(req, res)
+{
+    // todo later
 }
 
